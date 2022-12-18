a20d4077ab3a413d12e815a847e32c38ec30e525
Atmayanti <mayablue74@gmail.com>
2022-12-18 23:24:58 +0700

    fixing some bugs

--- lib/views/registerpage.dart
+++ lib/views/registerpage.dart
@@ -34,134 +34,136 @@ class _RegisterState extends State<Register> {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Form(
-            key: _registerKey,
-            child: Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Username is required';
-                      }
-                      return null;
-                    },
-                    controller: _usernameController,
-                    decoration: const InputDecoration(
-                      hintText: 'Username',
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Form(
+              key: _registerKey,
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Email is required';
-                      }
-                      return null;
-                    },
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      hintText: 'Email',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password is required';
-                      } else if (value.length < 8) {
-                        return 'Password should more than 8 characters';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password confirmation is required';
-                      } else if (value.length < 8) {
-                        return 'Password confirmation should more than 8 characters';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                    controller: _confirmpasswordController,
-                    decoration: const InputDecoration(
-                      hintText: 'Password Confirmation',
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 40, 0, 15),
-                    child: MaterialButton(
-                      onPressed: () async {
-                        if (_registerKey.currentState!.validate()) {
-                          bool response = await registerService.postRegister(
-                            _usernameController.text,
-                            _emailController.text,
-                            _passwordController.text,
-                            _confirmpasswordController.text,
-                          );
-                          if (response) {
-                            Navigator.of(context).popAndPushNamed('/login');
-                          } else {}
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Username is required';
                         }
+                        return null;
                       },
-                      color: const Color(0xFF424874),
-                      minWidth: 500,
-                      height: 60,
-                      child: Text(
-                        "Register",
-                        style:
-                            const TextStyle(color: const Color(0xFFF4EEFF), fontSize: 20),
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Username',
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Already have an account?"),
-                      TextButton(
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all(
-                            const Color(0xFF424874),
-                          ),
-                        ),
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(
-                            color: Color(0xFF424874),
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Email is required';
+                        }
+                        return null;
+                      },
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password is required';
+                        } else if (value.length < 8) {
+                          return 'Password must be at least 8 characters';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password confirmation is required';
+                        } else if (value.length < 8) {
+                          return 'Password confirmation must be at least 8 characters';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      controller: _confirmpasswordController,
+                      decoration: const InputDecoration(
+                        hintText: 'Password Confirmation',
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 40, 0, 15),
+                      child: MaterialButton(
+                        onPressed: () async {
+                          if (_registerKey.currentState!.validate()) {
+                            bool response = await registerService.postRegister(
+                              _usernameController.text,
+                              _emailController.text,
+                              _passwordController.text,
+                              _confirmpasswordController.text,
+                            );
+                            if (response) {
+                              Navigator.of(context).popAndPushNamed('/login');
+                            } else {}
+                          }
+                        },
+                        color: const Color(0xFF424874),
+                        minWidth: 500,
+                        height: 60,
+                        child: Text(
+                          "Register",
+                          style:
+                              const TextStyle(color: const Color(0xFFF4EEFF), fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Already have an account?"),
+                        TextButton(
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.all(
+                              const Color(0xFF424874),
+                            ),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Color(0xFF424874),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
