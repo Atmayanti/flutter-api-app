0a8e7d089967592772922f8571bb3709b98200d1
Atmayanti <mayablue74@gmail.com>
2022-12-18 12:29:28 +0700

    create list category

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:api_app/models/Category.dart';
+import 'package:api_app/services/Category_service.dart';
 import 'package:flutter/material.dart';
 import 'package:api_app/services/logout_service.dart';
 
@@ -10,10 +12,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  List<Category> listCategory = [];
+  final cs = CategoryService();
+
   final logout = LogoutService();
+
+  getData() async {
+    listCategory = await cs.getData();
+  }
 
   @override
   void initState() {
+    getData();
     super.initState();
   }
 
@@ -23,17 +33,51 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              logout.postDataLogout().then(
-                    (value) => Navigator.of(context).popAndPushNamed('/login'),
-                  );
-            },
-            child: const Text('Logout'),
-          ),
-        ],
+      body: Container(
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: cs.getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return ListView(
+                      children: (snapshot.data ?? [])
+                          .map(
+                            (e) => Card(
+                              margin: EdgeInsets.all(4.0),
+                              color: Colors.blueGrey[50],
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Text(e.name),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+            MaterialButton(
+              minWidth: 500,
+              height: 60,
+              child: Text(
+                "Logout",
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              color: Colors.blue,
+              onPressed: () {
+                logout.postDataLogout().then(
+                      (value) => Navigator.of(context).popAndPushNamed('/login'),
+                    );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
